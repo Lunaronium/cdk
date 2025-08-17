7fd37e4ded7854cb62c987e8246a933cea0bd690
Lunaronium <lunar.frontline@gmail.com>
2025-08-17 14:04:40 +0200

    Update ec2-stack.ts
    added second EC2 Instance within AZ 2. Rather than just one Instance in AZ 1.

--- lib/ec2-stack.ts
+++ lib/ec2-stack.ts
@@ -12,12 +12,13 @@ export class EC2Stack extends cdk.Stack {
     constructor(scope: Construct, id: string, props: EC2StackProps) {
         super(scope, id, props);
 
-    //EC2 Instance
+    //EC2 Instance 1 in AZ 1
 
-            const instance = new ec2.Instance(this, 'MyPrivateEC2', {
+            const instance1 = new ec2.Instance(this, 'MyPrivateEC2-AZ1', {
                 vpc: props.vpc,
                 vpcSubnets: {
-                    subnetType: ec2.SubnetType.PRIVATE_ISOLATED
+                    subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+                    availabilityZones: [props.vpc.availabilityZones[0]]
                 },
                 machineImage: new ec2.AmazonLinuxImage({
                     generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2
@@ -25,6 +26,21 @@ export class EC2Stack extends cdk.Stack {
                 instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO)
             })
 
-            cdk.Tags.of(instance).add('Name', 'MyPrivateEC2' )
+            cdk.Tags.of(instance1).add('Name', 'MyPrivateEC2-AZ1' )
+    //EC2 Instance 2 in AZ 2
+
+            const instance2 = new ec2.Instance(this, 'MyPrivateEC2-AZ1', {
+                vpc: props.vpc,
+                vpcSubnets: {
+                    subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+                    availabilityZones: [props.vpc.availabilityZones[1]]
+                },
+                machineImage: new ec2.AmazonLinuxImage({
+                    generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2
+                }),
+                instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO)
+            })
+
+            cdk.Tags.of(instance2).add('Name', 'MyPrivateEC2-AZ2' )
     }
 }
